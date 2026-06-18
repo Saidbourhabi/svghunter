9ed4c25890535483e8b9793b5242429549f17cda
Said-bourhabi <abousaid11araqi@gmail.com>
2026-06-18 02:15:32 +0100

    feat (app): create layouts pages routing config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/home/home"
+import NotFound from "./pages/NotFound"
+import Documentation from "./pages/documentation/documentation"
+import Header from "./layouts/Header"
+import Footer from "./layouts/Footer"
 
 function App() {
 
   return (
     <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="documentation" element={<Documentation />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
     </>
   )
 }
